48cbff3115ee821a477d32b7eef9e521c21bd653
Son Tran <datonst@gmail.com>
2024-12-25 09:28:50 +0700

    update

--- dev/main.tf
+++ dev/main.tf
@@ -17,13 +17,13 @@ resource "tls_private_key" "key_pair" {
   rsa_bits  = 4096
 }
 # Create the Key Pair
-resource "aws_key_pair" "key_pair" {
-  key_name   = "web-ec2-key-pair"
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
+# resource "aws_key_pair" "key_pair" {
+#   key_name   = "web-ec2-key-pair"
+#   public_key = tls_private_key.key_pair.public_key_openssh
+# }
 # Save file
 resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  filename = "key.pem"
   content  = tls_private_key.key_pair.private_key_pem
 }
 
@@ -33,23 +33,4 @@ data "http" "source_ip" {
 
 
 
-# module "appgw" {
-#   source = "../modules/regional/application-gateway"
 
-#   location            = local.region
-#   appgw_name            = "${local.prefix}-appgw"
-#   resource_group_name = module.resource_group.name
-#   subnet_id           = module.web-vnet.subnets["${local.prefix}-nat-subnet"].resource_id
-#   frontend_port       = 80
-#   backend_port        = 80
-#   sku_name = "Standard_v2"
-  
-# }
-
-
-# resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assoc" {
-#   count                   = 2
-#   network_interface_id    = azurerm_network_interface.nic[count.index].id
-#   ip_configuration_name   = "nic-ipconfig-${count.index+1}"
-#   backend_address_pool_id = var.backend_address_pool_id
-# }
